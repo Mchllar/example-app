de9bbdd216ee1c4387de6917645445d48ff8463d
Mchllar <mchllatieno@gmail.com>
2024-04-24 11:03:24 +0300

    Intention email

---
+++ resources/views/emails/intention.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Intention Email</title>
+</head>
+<body>
+    <p>Dear Administrator,</p>
+    <p>This is to inform you that student {{ $studentName }} (Student Number: {{ $studentNumber }}) has submitted their notice of intention to submit their thesis.</p> </br>
+    <p>Regards,</p>
+    
+</body>
+</html>
